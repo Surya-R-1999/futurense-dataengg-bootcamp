74e73d596bf8c7322cebafb1fd534ebf8da7ed52
Surya <suryar@futurensetech.com>
2023-03-07 15:17:35 +0530

    Files added

---
+++ Project/Spark_Streaming/structured_network_wordcount_windowed.py
@@ -0,0 +1,39 @@
+import sys
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
+from pyspark.sql.functions import window
+
+spark = SparkSession\
+  .builder\
+  .appName("StructuredNetworkWordCountWindowed")\
+  .getOrCreate()
+  
+  
+lines = spark\
+  .readStream\
+  .format('socket')\
+  .option('host', "localhost")\
+  .option('port', 9999)\
+  .option('includeTimestamp', 'true')\
+  .load()
+  
+  
+words = lines.select(
+explode(split(lines.value, ' ')).alias('word'),
+lines.timestamp
+ )
+ 
+windowedCounts = words.groupBy(
+window(words.timestamp,'30 seconds','10 seconds'),     # 30 -> window duration and 10 -> Slide duration
+words.word
+).count().orderBy('window')
+ 
+query = windowedCounts\
+.writeStream\
+.outputMode('complete')\
+.format('console') \
+.option("checkpointLocation", "checkpoint") \
+.option('truncate', 'false')\
+.start()
